7027e4f01ae2b2a7f6212f3775a37ba408e2f6a0
nv-ngoc <nv-ngoc@ruby-dev.jp>
2021-11-09 11:55:11 +0700

    Update

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,11 @@ class Category < ApplicationRecord
   def should_generate_new_friendly_id?
     name_changed? || super
   end
+
+  def normalize_friendly_id(value)
+    value = NonAccentVietnamese.new(value).call
+    value = value.parameterize
+    value = value[0...friendly_id_config.slug_limit] if friendly_id_config.slug_limit
+    value
+  end
 end

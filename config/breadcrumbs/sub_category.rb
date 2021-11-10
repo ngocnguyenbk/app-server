442196f30ec69f9ec046d011e7136e8d3b5fdb3a
nv-ngoc <nv-ngoc@ruby-dev.jp>
2021-11-10 16:39:06 +0700

    Fix bug

--- config/breadcrumbs/sub_category.rb
+++ config/breadcrumbs/sub_category.rb
@@ -13,7 +13,7 @@ crumb :sub_category_new do |category|
   parent :sub_categories, category
 end
 
-crumb :sub_categories do
+crumb :all_sub_categories do
   link t(".sub_categories")
   parent :root
 end

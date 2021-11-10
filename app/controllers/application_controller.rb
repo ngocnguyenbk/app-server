8de19e2c5ab012e17d55a4d7f45d93456f391bc2
nv-ngoc <nv-ngoc@ruby-dev.jp>
2021-11-10 17:57:58 +0700

    Fix header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :categories
+
+  def categories
+    @categories ||= Category.all.preload(:sub_categories)
+  end
 end

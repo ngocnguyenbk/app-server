536527f37ecae9f681cf6c9a9d59a912aacade63
nv-ngoc <nv-ngoc@ruby-dev.jp>
2021-11-12 16:47:58 +0700

    Add color

---
+++ app/helpers/link_to_helper.rb
@@ -0,0 +1,19 @@
+module LinkToHelper
+  def active_link_to(object, path, options = {})
+    options[:class] ||= ""
+    options[:class] += " nav-link fw-bold"
+    if current_page?(path)
+      options[:style] = "color: #{object.hex_code}"
+    else
+      options[:onMouseOver] = "this.style.color='#{object.hex_code}'"
+      options[:onMouseOut] = "this.style.color=''"
+    end
+    link_to object.name, path, options
+  end
+
+  def normal_link_to(object, path, options = {})
+    options[:class] ||= ""
+    options[:class] += " nav-link"
+    link_to object.name, path, options
+  end
+end

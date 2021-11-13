53ee8ed24270af1b6c495129b407108398312c1d
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2021-11-13 09:36:13 +0700

    Create Author

---
+++ app/models/author.rb
@@ -0,0 +1,13 @@
+class Author < ApplicationRecord
+  extend FriendlyId
+  prepend GenerateSlug
+
+  validates :full_name, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, if: :email?
+  validates :email, uniqueness: true, if: :email?
+
+  friendly_id :nickname, use: :slugged
+
+  alias_attribute :name, :nickname
+end

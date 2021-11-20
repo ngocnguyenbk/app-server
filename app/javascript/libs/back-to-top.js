e6bb680afcab7f9ed62adabfa930965179153864
ngocnguyenbk <nguyenvanngoc1996@gmail.com>
2021-11-20 22:18:24 +0700

    Add back to top

---
+++ app/javascript/libs/back-to-top.js
@@ -0,0 +1,29 @@
+function BackToTop() {
+  const module = this
+  module.btnBackToTop = document.getElementById('back-to-top')
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      module.btnBackToTop.style.display = 'block'
+    } else {
+      module.btnBackToTop.style.display = 'none'
+    }
+  }
+
+  function backToTop() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+  }
+
+  module.init = () => {
+    module.btnBackToTop.addEventListener('click', backToTop)
+    window.onscroll = () => {
+      scrollFunction()
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTop = new BackToTop()
+  backToTop.init()
+})
